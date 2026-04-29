56be04636d4aab1d469a1063b2d775be7cdbdd30
Gustavo Erlindo Dellajustina Demarchi <Gustavodemarchi13@gmail.com>
2026-04-28 21:48:22 -0300

    a

--- dicionario.pas
+++ dicionario.pas
@@ -1,19 +1,74 @@
 program pzim;
 	type
-		ptnodo_verbete = ^E_Dicionario;
+		Tverbete = ^E_Dicionario;
 		E_Dicionario = record
 			portugues : string;
 			ingles : string;
-			prox : ptnodo_verbete;
+			prox : Tverbete;
 		end; 
 	
-		Lista = ^E_lista;
+		Tlista = ^E_lista;
 		E_lista = record ;
-			anterior : lista ;
-			palavrachave : string;
+			ant : Tlista ; //anterior
+			key : string;
 			dicionario : E_dicionario ;
-			prox : lista ;
+			prox : Tlista ;
 		end;
+   	
+	function maior(a , b : string):boolean;
+	begin
+		maior := upcase(a) > upcase(b);	
+	end;
+
+	procedure criar_lista(lista: Tlista );
+	begin
+		lista:=nil;
+	end;
+
+	procedure adicionar_lista(var lista : Tlista ; chave : string);
+	var
+		aux , aux2 : Tlista;
+	begin
+		new(aux);
+		if
+			//memoria cheia
+		else
+		begin
+			while aux^.prox <> nil do
+				aux := aux^.prox;
+			
+			aux^.key := chave ;//			
+						
+			
+		end;		
+	end; 
+
+
+
+
+procedure portugues()
+		
+procedure adicionar_dicionario(dicionario : E_dicionario);
+var
+	aux , aux2 : Tlista;
+begin
+		new(aux);
+		if aux = nil then
+			begin
+				//memoria cheia
+			end
+		else
+		begin
+			if (dicionario = nil) then
+				begin
+					aux^.
+				end;
+			while aux^.prox <> nil do 
+				aux := aux^.prox
+			if aux^.prox = nil then
+		end;
+				
+end;
 			
 begin
 end.			
